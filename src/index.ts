e7764a3c659623ccd669de76472608da22f2e284
Kevin Nemec <kevin.nemec@resurs.se>
2025-01-15 14:08:11 +0100

    Hard code channel id to resurs bank channel

--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,29 @@ import { mainBotChannel } from "./util/core";
 createUser({
   name: "kevin.nemec",
   normalizedName: "Kevin Nemec",
-  slackId: "U088QAS0F16",
+  slackId: "U07NLT3FD7V",
 });
 */
 
 (async () => {
   await app.start();
   app.logger.info("Bot is running");
-  const { channels } = await app.client.users.conversations({});
-  const maybePingPongChannel = channels?.find((c) => c.name === "ping-pong");
 
-  if (!maybePingPongChannel) {
-    app.logger.info("Searching for ping-pong channel...");
+  const { channel } = await app.client.conversations.info({
+    channel: "C088U7R0NPN",
+  });
 
-    const pingPongChannel = await app.client.conversations
-      .list()
-      .then(({ channels }) =>
-        channels?.find((channel) => channel.name === "ping-pong")
-      );
-
-    if (!pingPongChannel || !pingPongChannel.id) {
-      app.logger.error(
-        "No ping-pong channel found in Slack workspace. Exiting app."
-      );
-      process.exit(1);
-    } else {
-      app.logger.info("Found ping-pong channel, joining channel.");
-      const channel = await app.client.conversations.join({
-        channel: pingPongChannel.id,
-      });
-      mainBotChannel.set(channel.channel!);
-      app.logger.info("Joined ping-pong channel.");
-    }
+  if (!channel) {
+    app.logger.error(
+      "No ping-pong channel found in Slack workspace. Exiting app."
+    );
+    process.exit(1);
   } else {
-    mainBotChannel.set(maybePingPongChannel);
+    app.logger.info("Found ping-pong channel, joining channel.");
+    await app.client.conversations.join({
+      channel: channel!.id!,
+    });
+    app.logger.info("Joined ping-pong channel.");
+    mainBotChannel.set(channel);
   }
 })();
